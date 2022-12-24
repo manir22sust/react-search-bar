4fd2c0caefeaf443adc6e3778702c962f4aef412
Manir Uddin <manir22sust@gmail.com>
2022-12-25 00:44:31 +0100

    search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Data from "./data/MOCK_DATA.json";
 
 function App() {
+  const [query, setQuery] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        type="text"
+        placeholder="Enter Post Title"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+
+      {Data.filter((post) => {
+        if (query === "") {
+          return post;
+        } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
+          return post;
+        }
+      }).map((post) => (
+        <div className="box" key={post.id}>
+          <p>{post.title}</p>
+          <p>{post.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
